907c16ce54babe5e4dede5cc121f6653a64ddf7b
Vitaly Zorin <VIZorin@stud.kpfu.ru>
2022-05-20 20:21:15 +0300

    Brute-force algorithms is done

--- src/knapsack/backtracking.cpp
+++ src/knapsack/backtracking.cpp
@@ -35,13 +35,29 @@ namespace assignment {
 
     // ... если текущая "польза" максимальна, обновляем наилучшую "пользу"
     if (profit > best_profit) {
-      // ...
+      best_profit = profit;
+      best_profit_mask = mask;
     }
 
     // рассматриваем следующий элемент
     index += 1;
 
     // ... рекурсивные вызовы со включением/исключением следующего элемента
+    const auto masked_profits = mask2elems(profits, mask);
+    int curr_profit = 0;
+    for (int one_profit : masked_profits) {
+      curr_profit += one_profit;
+    }
+    const auto masked_weights = mask2elems(weights, mask);
+    int curr_weight = 0;
+    for (int one_weight : masked_weights) {
+      curr_weight += one_weight;
+    }
+    solve(profits, weights, capacity, index, mask,
+          curr_weight, curr_profit, best_profit, best_profit_mask);
+    solve(profits, weights, capacity, index,
+          set_bit(mask, index), curr_weight + weights[index],
+          curr_profit + profits[index], best_profit, best_profit_mask);
   }
 
 }  // namespace assignment
